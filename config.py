9e289769561f78da7ceb341c6bbb082be43137df
Erfan Alimohammadi <erfan.aa@gmail.com>
2024-08-07 09:33:39 +0330

    Use top posts instead of last posts

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
-SUBREDDIT_NAMES_LIST = ["Politics", "PoliticsDiscussion"]
+SUBREDDIT_NAMES_LIST = ["Politics"]
 KEYWORDS_1_LIST = ["Donald", "Trump", "Republican"]
 KEYWORDS_2_LIST = ["Kamala", "Harris", "Democratic"]
+TOP_POSTS_TIME_FILTER = "week" # "all", "day", "hour", "month", "week", or "year"
 MAXIMUM_POSTS_PER_SUBREDDIT = 100
--- data_gatherer.py
+++ data_gatherer.py
@@ -2,9 +2,10 @@ import praw
 
 class DataGatherer:
 
-    def __init__(self, client_id: str, client_secret: str, subreddit_names_list: list[str], maximum_posts_per_subreddit: int):
+    def __init__(self, client_id: str, client_secret: str, subreddit_names_list: list[str], maximum_posts_per_subreddit: int, top_posts_time_filter: str):
         self.subreddit_names_list = subreddit_names_list
         self.maximum_posts_per_subreddit = maximum_posts_per_subreddit
+        self.top_posts_time_filter = top_posts_time_filter
         self.reddit_client = praw.Reddit(client_id=client_id,
                                          client_secret=client_secret,
                                          user_agent="user_agent")
@@ -16,21 +17,25 @@ class DataGatherer:
 
         for subreddit_name in self.subreddit_names_list:
             subreddit = self.reddit_client.subreddit(subreddit_name)
-            latest_posts = subreddit.new(limit=self.maximum_posts_per_subreddit)
+            top_posts = subreddit.top(time_filter=self.top_posts_time_filter)
 
-            for post in latest_posts:
-                retrieved_posts_count += 1
-                print("retrieved_posts_count:", retrieved_posts_count)
+            current_subreddit_retrieved_posts_count = 0
 
+            for post in top_posts:
                 post.comments.replace_more(limit=0)
 
                 for comment in post.comments.list():
                     comment_author = comment.author
-
                     if comment_author and comment_author.name != "AutoModerator":
                         if comment_author not in self.comments_list_by_author:
                             self.comments_list_by_author[comment_author.name] = [comment.body]
                         else:
                             self.comments_list_by_author[comment_author.name].append(comment.body)
+                
+                retrieved_posts_count += 1
+                current_subreddit_retrieved_posts_count += 1
+                print("Retrieved posts count:", retrieved_posts_count)
+                if current_subreddit_retrieved_posts_count == self.maximum_posts_per_subreddit:
+                    break
             
         return self.comments_list_by_author
